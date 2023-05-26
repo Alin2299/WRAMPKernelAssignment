dd0add5171e203a839e79b45a9079b3159cf349a
Alin2299 <andrewlin125@gmail.com>
2023-05-26 20:59:53 +1200

    Finished writing for base 16 and started testing

--- parallel_task.c
+++ parallel_task.c
@@ -1,22 +1,47 @@
 // Program that continually reads switches' value, and writes to the SSDs as a 4-digit number
 // and allows the user to change the format of that number
 
-// Include the necessary WRAMP header file
+// Include the necessary header files
 #include "wramp.h"
+
+// Int representing the value of the switches
+int switches = 0;
+
+// Int variables that represent whether to display numbers in base 10 or 16 (1 is true, 0 is false)
+int displayBase10 = 1;
+int displayBase16 = 0;
 
 /**
  * Main subroutine
  * */ 
 void main()
 {
-    // Int representing the value of the switches
-    int switches = 0;
 
     // Infinite (while) loop
     while(1)
     {
         // Get current switch value from parallel switches register
         switches = WrampParallel->Switches;
+
+        // If button 0 pushed, display numbers in base 10 
+        if (WrampParallel->Buttons == 1)
+        {
+            displayBase10 = 1;
+            displayBase16 = 0;
+        }
+
+        // Else if button 1 pushed, display numbers in base 16
+        else if (WrampParallel->Buttons == 2)
+        {
+            displayBase10 = 0;
+            displayBase16 = 1;
+        }
+
+        // Else if button 2 pushed, exit program by returning 
+        else if (WrampParallel->Buttons == 4)
+            return;
+
+        displayChar();
     }
 }
 
@@ -25,5 +50,19 @@ void main()
  * */ 
 void displayChar()
 {
+    if (displayBase10 == 1)
+    {
+        
+    }
 
+    else if (displayBase16 == 1)
+    {
+        char hexSwitchesValue[5];
+        sprintf(hexSwitchesValue, "%04X", switches);
+
+        WrampParallel->LowerRightSSD = hexSwitchesValue[1];
+        WrampParallel->LowerLeftSSD = hexSwitchesValue[2];
+        WrampParallel->UpperRightSSD = hexSwitchesValue[3];
+        WrampParallel->UpperLeftSSD = hexSwitchesValue[4];
+    }
 }
